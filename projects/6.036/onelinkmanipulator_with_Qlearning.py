e76989975fe453c40f32c0dbc81ee0200b7248d6
Alexandre Girard <agirard.mit.edu>
2016-05-09 09:24:42 -0400

    created new file organization for 6.036 project

---
+++ projects/6.036/onelinkmanipulator_with_Qlearning.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 12:50:34 2016
+
+@author: alex
+"""
+
+from AlexRobotics.dynamic import Manipulator   as M
+from AlexRobotics.control import DPO           as DPO
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define dynamic system
+R  =  M.OneLinkManipulator()
+
+# Define controller
+cost_function     = 'quadratic'
+QLearningAlgo     = DPO.QLearning1DOF( R , cost_function )
+
+
+path = 'data/'
+name = 'R1_Qlearning' + cost_function 
+
+n_steps = 1000
+#n_steps = 200
+
+QLearningAlgo.first_step()
+QLearningAlgo.load_data( name )
+
+x0 = np.array( [ -3 , 0 ] )
+QLearningAlgo.x0 = x0
+
+QLearningAlgo.training( n_steps , True , True )
+
+QLearningAlgo.save_data( name )
+
+QLearningAlgo.plot_J()
+
+R.plotAnimation( x0 )
---
+++ projects/6.036/onelinkmanipulator_with_ValueIteration.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  6 14:45:38 2016
+
+@author: alex
+"""
+
+from AlexRobotics.dynamic import Manipulator   as M
+from AlexRobotics.control import DPO           as DPO
+
+import matplotlib.pyplot as plt
+
+# Define dynamic system
+R  =  M.OneLinkManipulator()
+
+# Define controller
+cost_function = 'quadratic'
+ValueIterationAlgo     = DPO.ValueIteration1DOF( R , cost_function )
+
+path = 'data/'
+name = path + 'R1' + cost_function 
+
+n_steps = 1
+#n_steps = 200
+
+ValueIterationAlgo.first_step()
+ValueIterationAlgo.load_data( name )
+ValueIterationAlgo.compute_steps( n_steps )
+ValueIterationAlgo.save_data( name ) 
+
+# Plot Value Iteration Results
+ValueIterationAlgo.plot_J_nice( 1.5 )
+ValueIterationAlgo.plot_raw()
+
+# Assign Controller
+ValueIterationAlgo.assign_interpol_controller() 
+
+# Ploting a trajectory
+u_OL = [0]
+x0   = [3,0]
+tf   = 10
+R.plotAnimation( x0 , tf )
+R.phase_plane_trajectory( u_OL , x0 , tf , True , True , True, True )
+
+# Time plot
+R.Sim.plot_CL()
+
+# Hold figures alive
+plt.show()
