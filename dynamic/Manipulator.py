b4110f0fba3309dcdc18ab229e636f55e8caff34
alx87grd <alx87grd@gmail.com>
2016-08-17 13:41:59 -0400

    time scalling fixed for manipulator animations

--- dynamic/Manipulator.py
+++ dynamic/Manipulator.py
@@ -316,6 +316,25 @@ class TwoLinkManipulator( RDDS.DynamicSystem ) :
         
         return F_ext
         
+        
+    ##############################
+    def e_potential(self, q = np.zeros(2) ):
+        """ Compute potential energy of manipulator """  
+        
+        [c1,s1,c2,s2,c12,s12] = self.trig( q )
+        
+        g1 = (self.m1 * self.lc1 + self.m2 * self.l1 ) * self.g
+        g2 = self.m2 * self.lc2 * self.g
+        
+        e_p = g1 * c1 + g2 * c12        
+        
+        return e_p
+        
+        
+    ##########################################################################
+    # Based functions : No need to modify the next functions for custom robots
+    ##########################################################################
+        
     
     ##############################
     def B(self, q = np.zeros(2) ):
@@ -433,20 +452,6 @@ class TwoLinkManipulator( RDDS.DynamicSystem ) :
         
         return e_k
         
-        
-    ##############################
-    def e_potential(self, q = np.zeros(2) ):
-        """ Compute potential energy of manipulator """  
-        
-        [c1,s1,c2,s2,c12,s12] = self.trig( q )
-        
-        g1 = (self.m1 * self.lc1 + self.m2 * self.l1 ) * self.g
-        g2 = self.m2 * self.lc2 * self.g
-        
-        e_p = g1 * c1 + g2 * c12        
-        
-        return e_p
-        
     
     ##############################
     def energy_values(self, x = np.zeros(4)  ):
@@ -521,29 +526,41 @@ class TwoLinkManipulator( RDDS.DynamicSystem ) :
         
         return fig , ax, line
         
-    
+        
+        
     #############################
-    def plotAnimation(self, x0 = np.array([0,0,0,0]) , tf = 10 , n = 201 , solver = 'ode' ,  save = False , file_name = 'RobotSim'  ):
-        """ Simulate and animate robot """
+    def computeSim(self, x0 = np.array([0,0,0,0]) , tf = 10 , n = 1001 , solver = 'ode' ):
+        """ Simulate the robot """
         
         
-        # Integrate EoM
         self.Sim    = RDDS.Simulation( self , tf , n , solver )
         self.Sim.x0 = x0
         
+        # Integrate EoM
         self.Sim.compute()
         
-        # Compute pts localization
-        self.PTS = np.zeros(( self.n_pts , self.dim_pts , n ))
         
-        for i in xrange(n):
+    ##############################
+    def animateSim(self, time_factor_video =  1.0 , save = False , file_name = 'RobotSim' ):
+        """ 
+        Show Animation of the simulation 
+        ----------------------------------
+        time_factor_video < 0 --> Slow motion video        
+        
+        """  
+        
+        # Compensate for slightly slower ploting than it should:
+        time_factor_video = time_factor_video * 1.15
+        
+        # Compute pts localization
+        self.PTS = np.zeros(( self.n_pts , self.dim_pts , self.Sim.n ))
+        
+        for i in xrange( self.Sim.n ):
             
             [ q , dq ]      = self.x2q(  self.Sim.x_sol_CL[i,:]  )
             self.PTS[:,:,i] = self.fwd_kinematic( q )  # Forward kinematic
             
-            
-        # figure
-            
+        # Figure
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
         self.ax.grid()
@@ -552,20 +569,38 @@ class TwoLinkManipulator( RDDS.DynamicSystem ) :
         self.time_template = 'time = %.1fs'
         self.time_text = self.ax.text(0.05, 0.9, '', transform=self.ax.transAxes)
             
-        inter = int( n / 8. )
-        #interval=25 with n=201
-           
-        # blit=True option crash on mac
-        #self.ani = animation.FuncAnimation( self.fig, self.__animate__, n, interval = inter, blit=True, init_func=self.__ani_init__)
-        self.ani = animation.FuncAnimation( self.fig, self.__animate__, n, interval = inter, init_func=self.__ani_init__)
+        inter      =  40.             # ms --> 25 frame per second
+        frame_dt   =  inter / 1000. 
         
+        if ( frame_dt * time_factor_video )  < self.Sim.dt :
+            # Simulation is slower than video
+            self.skip_steps = 1                                         # don't skip steps
+            inter           = self.Sim.dt * 1000. / time_factor_video   # adjust frame speed to simulation
+            n_frame         = self.Sim.n
+            
+        else:
+            # Simulation is faster than video
+            self.skip_steps =  int( frame_dt / self.Sim.dt * time_factor_video ) # --> number of simulation frame to skip between video frames
+            n_frame         =  int( self.Sim.n / self.skip_steps )               # --> number of video frames
+        
+        # ANIMATION
+        # blit=True option crash on mac
+        #self.ani = animation.FuncAnimation( self.fig, self.__animate__, n_frame , interval = inter, blit=True, init_func=self.__ani_init__)
+        self.ani = animation.FuncAnimation( self.fig, self.__animate__, n_frame , interval = inter , init_func=self.__ani_init__)
         
         if save:
             self.ani.save( file_name + '.mp4' ) # , writer = 'mencoder' )
             
-        plt.show()
         
-        return self.PTS
+        
+    
+    #############################
+    def plotAnimation(self, x0 = np.array([0,0,0,0]) , tf = 10 , n = 1001 , solver = 'ode' ,  save = False , file_name = 'RobotSim'  ):
+        """ Simulate and animate robot """
+        
+        self.computeSim( x0 , tf , n , solver )
+        
+        self.animateSim( 1.0 , save , file_name )
         
         
         
@@ -576,22 +611,22 @@ class TwoLinkManipulator( RDDS.DynamicSystem ) :
     
     
     def __animate__(self,i):
-        thisx = self.PTS[:, self.axis_to_plot[0] ,i]
-        thisy = self.PTS[:, self.axis_to_plot[1] ,i]
+        thisx = self.PTS[:, self.axis_to_plot[0] , i * self.skip_steps ]
+        thisy = self.PTS[:, self.axis_to_plot[1] , i * self.skip_steps ]
     
         self.line.set_data(thisx, thisy)
-        self.time_text.set_text(self.time_template % (i*0.05))
+        self.time_text.set_text(self.time_template % ( i * self.skip_steps * self.Sim.dt ))
         return self.line, self.time_text
         
     def __animateStop__(self,i):
         
-        if i > 198: # To close window
+        if i > 198: # Hack To close window used in reinforcement learning algo
             plt.close()
-        thisx = self.PTS[:, self.axis_to_plot[0] ,i]
-        thisy = self.PTS[:, self.axis_to_plot[1] ,i]
+        thisx = self.PTS[:, self.axis_to_plot[0] , i * self.skip_steps ]
+        thisy = self.PTS[:, self.axis_to_plot[1] , i * self.skip_steps ]
     
         self.line.set_data(thisx, thisy)
-        self.time_text.set_text(self.time_template % (i*0.05))
+        self.time_text.set_text(self.time_template % ( i * self.skip_steps * self.Sim.dt ))
         return self.line, self.time_text
 
             
@@ -602,8 +637,15 @@ class TwoLinkManipulator( RDDS.DynamicSystem ) :
 '''
 
 
-class OneLinkManipulator( RDDS.DynamicSystem ) :
-    """ 2DOF Manipulator Class """
+class OneLinkManipulator( TwoLinkManipulator ) :
+    """ 
+    1DOF Manipulator Class
+    -----------------------------------
+    Pendulum used as exemple
+    
+    some base function are overloaded with the scalar version when necessarly
+    
+    """
     
     
     ############################
@@ -655,6 +697,9 @@ class OneLinkManipulator( RDDS.DynamicSystem ) :
         self.g = 9.81
         
         self.d1 = 0
+        
+        # Total length
+        self.lw = 1
         
         
     ##############################
@@ -857,36 +902,10 @@ class OneLinkManipulator( RDDS.DynamicSystem ) :
         
         ddq = np.dot( 1./H ,  ( np.dot( B , e ) - np.dot( C , dq ) - np.dot( D , dq ) - G  ) )
         
+        #  TODO include external forces for 1-DOF
+        
         return ddq
-        
-        
-    #############################
-    def fc(self, x = np.zeros(2) , u = np.zeros(1) , t = 0 ):
-        """ 
-        Continuous time function evaluation
-        
-        INPUTS
-        x  : state vector             n x 1
-        u  : control inputs vector    m x 1
-        t  : time                     1 x 1
-        
-        OUPUTS
-        dx : state derivative vectror n x 1
-        
-        """
-        
-        dx = np.zeros(self.n) # State derivative vector
-        
-        q  = x[0]
-        dq = x[1]
-        
-        ddq = self.ddq( q , dq , u[0] )
-        
-        dx[0] = dq
-        dx[1] = ddq
-        
-        return dx
-        
+                
         
     #############################
     def x2q( self, x = np.zeros(2) ):
@@ -930,104 +949,6 @@ class OneLinkManipulator( RDDS.DynamicSystem ) :
         e_p = g1 * c1   
         
         return e_p
-        
-        
-    ##############################
-    def energy_values(self, x = np.zeros(1)  ):
-        """ Compute energy values of manipulator """ 
-        
-        q  = x[0]
-        dq = x[1]
-        
-        e_k = self.e_kinetic( q , dq )
-        e_p = self.e_potential( q )
-        e   = e_k + e_p
-        
-        return [ e , e_k , e_p ]
-        
-        
-    #############################
-    def show(self, q):
-        """ Plot figure of configuration q """
-        
-        fig = plt.figure()
-        ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
-        ax.grid()
-        
-        pts = self.fwd_kinematic( q )
-        
-        line = ax.plot( pts[:,0], pts[:,1], 'o-', lw=(self.l1) )
-        
-        plt.show()
-        
-        return fig , ax, line
-        
-  
-    
-    #############################
-    def plotAnimation(self, x0 = np.array([0,0]) , tf = 10 , n = 201 ,  solver = 'ode' , save = False , file_name = 'RobotSim' ):
-        """ Simulate and animate robot """
-        
-        # Integrate EoM
-        self.Sim    = RDDS.Simulation( self , tf , n  , solver  )
-        self.Sim.x0 = x0
-        
-        self.Sim.compute()
-        
-        # Compute pts localization
-        self.PTS = np.zeros((2,2,n))
-        
-        for i in xrange(n):
-            self.PTS[:,:,i] = self.fwd_kinematic( self.Sim.x_sol_CL[i,0] ) # Forward kinematic
-            
-        # Figure            
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
-        self.ax.grid()
-        
-        self.line, = self.ax.plot([], [], 'o-', lw=2)
-        self.time_template = 'time = %.1fs'
-        self.time_text = self.ax.text(0.05, 0.9, '', transform=self.ax.transAxes)
-
-        inter = int( n / 8. )
-        #interval=25 with n=201
-                    
-        # blit=True option crash on mac
-        #self.ani = animation.FuncAnimation( self.fig, self.__animate__, n, interval = inter , blit=True, init_func=self.__ani_init__)
-        self.ani = animation.FuncAnimation( self.fig, self.__animate__, n, interval = inter, init_func=self.__ani_init__)
-        
-        if save:
-            self.ani.save( file_name + '.mp4' ) #, writer = 'mencoder' )
-            
-        plt.show()
-        
-        return self.PTS
-        
-    def __ani_init__(self):
-        self.line.set_data([], [])
-        self.time_text.set_text('')
-        return self.line, self.time_text
-    
-    
-    def __animate__(self,i):
-        thisx = self.PTS[:,0,i]
-        thisy = self.PTS[:,1,i]
-    
-        self.line.set_data(thisx, thisy)
-        self.time_text.set_text(self.time_template % (i*0.05))
-        return self.line, self.time_text
-        
-    def __animateStop__(self,i):
-        
-        if i > 198: # To close window
-            plt.close()
-        thisx = self.PTS[:,0,i]
-        thisy = self.PTS[:,1,i]
-    
-        self.line.set_data(thisx, thisy)
-        self.time_text.set_text(self.time_template % (i*0.05))
-        return self.line, self.time_text
-        
         
 
 
@@ -1359,21 +1280,25 @@ class ThreeLinkManipulator( TwoLinkManipulator ) :
         plt.show()
         
         
+    ##############################
+    def animate3DSim(self, time_factor_video =  1.0 , save = False , file_name = 'RobotSim' ):
+        """ 
+        Show Animation of the simulation 
+        ----------------------------------
+        self.computeSim() need to be run before
         
-    #############################
-    def plot3DAnimation(self, x0 = np.array([0,0,0,0,0,0]) , tf = 10 , n = 201 ,  solver = 'ode' , save = False , file_name = 'RobotSim' ):
-        """ Simulate and animate robot """
+        time_factor_video < 0 --> Slow motion video        
         
-        # Integrate EoM
-        self.Sim    = RDDS.Simulation( self , tf , n  , solver  )
-        self.Sim.x0 = x0
+        """  
         
-        self.Sim.compute()
-
+        # Compensate for slightly slower ploting than it should:
+        time_factor_video = time_factor_video * 1.15
+        
         # Compute pts localization
-        self.PTS = np.zeros(( self.n_pts , self.dim_pts , n ))
+        self.PTS = np.zeros(( self.n_pts , self.dim_pts , self.Sim.n ))
         
-        for i in xrange(n):
+        for i in xrange( self.Sim.n ):
+            
             [ q , dq ]      = self.x2q(  self.Sim.x_sol_CL[i,:]  )
             self.PTS[:,:,i] = self.fwd_kinematic( q )  # Forward kinematic
             
@@ -1386,8 +1311,6 @@ class ThreeLinkManipulator( TwoLinkManipulator ) :
         self.time_template = 'time = %.1fs'
         self.time_text = self.ax.text(0, 0, 0, '', transform=self.ax.transAxes)
         
-        inter = int( n / 4. )        
-        
         # Setting the axes properties
         self.ax.set_xlim3d([ - self.lw / 2. , self.lw / 2. ])
         self.ax.set_xlabel('X')
@@ -1396,28 +1319,50 @@ class ThreeLinkManipulator( TwoLinkManipulator ) :
         self.ax.set_zlim3d([- self.lw / 2. , self.lw / 2.])
         self.ax.set_zlabel('Z')
         self.ax.set_title('3D Robot Animation')
-                    
-        self.ani = animation.FuncAnimation( self.fig, self.__animate_3D__, n, interval=inter, blit=False )
+        
+        inter      =  40.             # ms --> 25 frame per second
+        frame_dt   =  inter / 1000. 
+        
+        if ( frame_dt * time_factor_video ) < self.Sim.dt :
+            # Simulation is slower than video
+            self.skip_steps = 1                                         # don't skip steps
+            inter           = self.Sim.dt * 1000. / time_factor_video   # adjust frame speed to simulation
+            n_frame         = self.Sim.n
+            
+        else:
+            # Simulation is faster than video
+            self.skip_steps =  int( frame_dt / self.Sim.dt * time_factor_video ) # --> number of simulation frame to skip between video frames
+            n_frame         =  int( self.Sim.n / self.skip_steps )               # --> number of video frames
+        
+        
+        self.ani = animation.FuncAnimation( self.fig, self.__animate_3D__, n_frame, interval=inter, blit=False )
                     
         
         if save:
             self.ani.save( file_name + '.mp4' ) #, writer = 'mencoder' )
-            
-        plt.show()
         
-        return self.PTS
         
-    
+        
+    #############################
+    def plot3DAnimation(self, x0 = np.array([0,0,0,0,0,0]) , tf = 10 , n = 1001 ,  solver = 'ode' , save = False , file_name = 'RobotSim' ):
+        """ Simulate and animate robot """
+        
+        self.computeSim( x0 , tf , n , solver )
+        
+        self.animate3DSim( 1.0 , save , file_name )
+        
+        
+    ##############################
     def __animate_3D__(self,i):
-        thisx = self.PTS[:,0,i]
-        thisy = self.PTS[:,1,i]
-        thisz = self.PTS[:,2,i]
+        thisx = self.PTS[:,0, i * self.skip_steps]
+        thisy = self.PTS[:,1, i * self.skip_steps]
+        thisz = self.PTS[:,2, i * self.skip_steps]
     
         #self.line.set_data(thisx, thisy, thisz) # not working for 3D
         self.line.set_data(thisx, thisy)
         self.line.set_3d_properties(thisz)
         
-        self.time_text.set_text(self.time_template % (i*0.05))
+        self.time_text.set_text(self.time_template % (i * self.skip_steps * self.Sim.dt))
         return self.line, self.time_text
         
 
--- examples/onelinkmanipulator_with_RRT.py
+++ examples/onelinkmanipulator_with_RRT.py
@@ -41,7 +41,7 @@ RRT.traj_ctl_kd   = 10
 tf = RRT.time_to_goal + 5
 n = int( tf / 0.05 ) + 1
 R.plotAnimation( x_start , tf , n , solver = 'euler' )
-R.phase_plane_trajectory([0],x_start,tf,True,False,False,True)
+R.Sim.phase_plane_trajectory(True,False,False,True)
 RRT.plot_2D_Tree()
 R.Sim.plot_CL()
 
