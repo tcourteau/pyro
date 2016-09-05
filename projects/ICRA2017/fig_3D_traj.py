ed59bca77ae48ea761b6721c96c1bae88e44083f
alx87grd <alx87grd@gmail.com>
2016-09-05 17:51:32 -0400

    improved 3D sim figure

--- projects/ICRA2017/fig_3D_traj.py
+++ projects/ICRA2017/fig_3D_traj.py
@@ -52,12 +52,13 @@ RRT.dt                    = 0.15
 RRT.goal_radius           = 0.6
 RRT.alpha                 = 0.8
 RRT.max_nodes             = 25000
-RRT.max_solution_time     = 5
+RRT.max_solution_time     = 3
 
 # Dynamic plot
 RRT.dyna_plot             = True
 RRT.dyna_node_no_update   = 1000
 
+RRT.low_pass_filter.set_freq_to( fc = 2.0 , dt = RRT.dt )
 
 
 if ReComputeTraj:
@@ -69,6 +70,15 @@ if ReComputeTraj:
 else:
     
     RRT.load_solution( name_traj  )
+    
+
+#RRT.plot_open_loop_solution()
+#RRT.plot_open_loop_solution_acc()
+
+RRT.solution_smoothing()
+
+#RRT.plot_open_loop_solution()
+#RRT.plot_open_loop_solution_acc()
 
 
 # Assign controller
@@ -79,8 +89,10 @@ R.ctl               = CTC_controller.ctl
 
 CTC_controller.w0           = 1.0
 CTC_controller.zeta         = 0.7
-CTC_controller.traj_ref_pts = 'closest'
-CTC_controller.n_gears      = 2
+CTC_controller.traj_ref_pts = 'interpol'
+CTC_controller.n_gears      = 8
+CTC_controller.hysteresis   = True
+CTC_controller.hys_level    = 3
 
 """ Simulation and plotting """
 
@@ -90,11 +102,23 @@ n  = int( np.round( tf / 0.01 ) ) + 1
 R.computeSim( x_start , tf  , n , solver = 'euler' )
 
 # Plot
+
+R.Sim.fontsize = 7
+
+
 R.Sim.plot_CL('x') 
 R.Sim.plot_CL('u')
 
-R.Sim.plots[3].set_yticks( [0,1] )
-R.Sim.plots[3].set_ylim( -0.5,1.5 )
+R.Sim.plots[0].set_ylim( -12,12 )
+R.Sim.plots[1].set_ylim( -12,12 )
+R.Sim.plots[2].set_ylim( -12,12 )
+R.Sim.plots[0].set_yticks( [-10,0,10] )
+R.Sim.plots[1].set_yticks( [-10,0,10] )
+R.Sim.plots[2].set_yticks( [-10,0,10]  )
+R.Sim.plots[3].set_yticks( [0,8] )
+R.Sim.plots[3].set_ylim( -0.5,8.5 )
+R.Sim.plots[3].set_xlim( -0,6 )
+
 R.Sim.fig.canvas.draw()
 
 if save_fig:
