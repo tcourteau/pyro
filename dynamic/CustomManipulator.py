0efc636af134ce87f8a932a29eb804b2a2672840
alx87grd <alx87grd@gmail.com>
2016-11-14 21:04:41 -0500

    added 3rd DoF to Boeing Arm

--- dynamic/CustomManipulator.py
+++ dynamic/CustomManipulator.py
@@ -23,7 +23,7 @@ class BoeingArm( HM.HybridThreeLinkManipulator ) :
         HM.HybridThreeLinkManipulator.__init__(self, n , m )
         
         # Ploting param
-        self.n_pts   = 9 # number of pts to plot on the manipulator 
+        self.n_pts   = 10 # number of pts to plot on the manipulator 
         self.dim_pts = 3 # number of dimension for each pts 
         self.axis_to_plot = [0,2]  # axis to plot for 2D plot
         
@@ -57,7 +57,10 @@ class BoeingArm( HM.HybridThreeLinkManipulator ) :
         
         
         # Second link
-        self.l1 = 0.5  # Length of link 1
+        self.l1 = 0.3  # Length of link 1
+        
+        # Third Link
+        self.l2 = 0.3
         
         
         self.lw = 1
@@ -127,7 +130,14 @@ class BoeingArm( HM.HybridThreeLinkManipulator ) :
         c01     = np.cos( theta_0 + theta_1 )
         
         link1 = [ theta_1 , s1 , c1 , s01 , c01 ]
-        link2 = []
+        
+        theta_2 = q[2]
+        s2      = np.sin( theta_2 )
+        c2      = np.cos( theta_2 )
+        s012    = np.sin( theta_0 + theta_1 + theta_2 )
+        c012    = np.cos( theta_0 + theta_1 + theta_2 )
+        
+        link2 = [ theta_2 , s2 , c2 , s012 , c012 ]
         
         
         return [base,link1,link2]
@@ -154,13 +164,21 @@ class BoeingArm( HM.HybridThreeLinkManipulator ) :
         p6 = [ 0 , 0 , 0 ]
         p7 = [ self.b[0] * s0 , 0 , self.b[0] * c0 ]
         
-        ### Seconde DOF ###
+        ### Second DOF ###
         
         [ theta_1 , s1 , c1 , s01 , c01 ] = link1
         
         p8 = [ self.l1 * s01 + p7[0] , 0 , self.l1 * c01 + p7[2] ]
         
-        return np.array([p0,p1,p2,p3,p4,p5,p6,p7,p8])
+        
+        ### Third DOF ###
+        
+        [ theta_2 , s2 , c2 , s012 , c012 ] = link2
+        
+        p9 = [ self.l2 * s012 + p8[0] , 0 , self.l2 * c012 + p8[2] ]
+        
+        
+        return np.array([p0,p1,p2,p3,p4,p5,p6,p7,p8,p9])
         
                     
     ##############################
@@ -640,5 +658,5 @@ if __name__ == "__main__":
     
     R  = TestPendulum()
     
-    BA.plotAnimation( [0.1,1.2,0,0,0,0])
+    BA.plotAnimation( [0.1,1.2,-0.5,0,0.1,0.1])
     
