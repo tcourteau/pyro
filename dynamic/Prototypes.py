f388840ce86651cdca87fe6c088ad5729c97478b
Alex <alx87grd@gmail.com>
2016-11-18 10:57:20 -0500

    real 2-dof robot model

--- dynamic/Prototypes.py
+++ dynamic/Prototypes.py
@@ -11,6 +11,10 @@ Created on Thu Oct 20 13:56:13 2016
 from AlexRobotics.dynamic  import Hybrid_Manipulator   as HM
 import numpy as np
 
+#################################################################################################################
+###########   Single DSDM    ####################################################################################
+#################################################################################################################
+
 
 class SingleRevoluteDSDM( HM.HybridOneLinkManipulator ) :
     """  """
@@ -105,6 +109,117 @@ class SingleRevoluteDSDM( HM.HybridOneLinkManipulator ) :
         """ """        
         return self.ext_cst_force 
         
+        
+        
+        
+#################################################################################################################
+###########   Two planar DSDM    ################################################################################
+#################################################################################################################
+
+
+class TwoPlanarSerialDSDM( HM.HybridTwoLinkManipulator ) :
+    """  """
+
+    ############################
+    def __init__(self):
+        
+        HM.HybridTwoLinkManipulator.__init__(self )
+        
+        self.ubar = np.array([0,0,0])
+        
+        self.x_ub = np.array([ + 3 * np.pi , + 3 * np.pi ,  15 ,  15 ])
+        self.x_lb = np.array([ - 1 * np.pi , - 1 * np.pi , -15 , -15 ])
+        self.u_ub = np.array([ + 0.5 ,  + 0.5 , 500 ])
+        self.u_lb = np.array([ - 0.5 ,  - 0.5 , 0   ])
+        
+        # High-force Max speed
+        self.dq_max_HF = 0.9 # [rad/sec]
+        
+        self.setparams()
+        
+    #############################
+    def isavalidinput(self , x , u):
+        """ check if u is in the domain """
+        
+        ans = False # set to True if something is bad
+        
+        for i in xrange(self.m):
+            ans = ans or ( u[i] < self.u_lb[i] )
+            ans = ans or ( u[i] > self.u_ub[i] )
+            
+        # add w_max constraint
+        
+        # if High-force mode
+        if u[1] == self.R[1] :
+            
+            dq1 = x[2]
+            dq2 = x[3]
+            
+            ans = ans or ( dq1 >  self.dq_max_HF )
+            ans = ans or ( dq1 < -self.dq_max_HF )
+            ans = ans or ( dq2 >  self.dq_max_HF )
+            ans = ans or ( dq2 < -self.dq_max_HF )
+            
+        return not(ans)
+        
+        
+    #############################
+    def setparams(self):
+        """ Set model parameters here """
+        
+        self.l1  = 0.25
+        self.lc1 = 0.1
+        self.l2  = 0.25
+        self.lc2 = 0.1
+        
+        self.m1 = 1
+        self.I1 = 0      # neglect
+        self.m2 = 0.25
+        self.I2 = 0      # neglect
+        
+        # load
+        self.M  = 1.0
+        
+        self.g = 9.81 
+        
+        self.d1 = 0.05
+        self.d2 = 0.05
+                
+        # Total length
+        self.lw  = (self.l1+self.l2) * 1.1
+        
+        self.setActuatorParams()
+        
+    #############################
+    def setActuatorParams(self ):
+        """ Set actuators parameters here """
+        
+        # Actuator damping coef
+        self.Da = np.diag( [ 0.00002 , 0.00002 ] ) * 0.1
+        
+        # Actuator inertia coef
+        
+        I_m = 15 # gcm2
+        
+        self.Ia = np.diag( [ I_m , I_m ] ) * 0.001 * ( 0.01 **2 )
+        
+        # Gear ratio
+        # Actuator elbow
+        a1r1   = 72.0
+        a1r2   = 1225
+        # Actuator wrist
+        a2r1   = 23.2   # 5.8 * 4
+        a2r2   = 474.0  # 123 * 52/18 *4/3
+        
+        R1 = np.diag([ a1r1 , a2r1 ])
+        R2 = np.diag([ a1r2 , a2r1 ])
+        R3 = np.diag([ a1r1 , a2r2 ])
+        R4 = np.diag([ a1r2 , a2r2 ])
+        
+        self.R = [ R1 , R2 , R3 , R4 ]
+        
+    
+        
 
 
 '''
@@ -117,8 +232,8 @@ class SingleRevoluteDSDM( HM.HybridOneLinkManipulator ) :
 if __name__ == "__main__":     
     """ MAIN TEST """
     
-    R = SingleRevoluteDSDM()
+    R1 = SingleRevoluteDSDM()
     
-    R.ext_cst_force = 0
+    R2 = TwoPlanarSerialDSDM()
     
-    R.plotAnimation([3.14,0])
+    R2.plotAnimation([ 3.14 , -3.0 , 0 ,0 ])
