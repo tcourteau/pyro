88892647a52c0b7a10f6ff7a4d955806c90c2473
Alexandre Girard <alx87grd@gmail.com>
2019-04-28 21:06:10 -0400

    demo all working

--- examples/demo_simple_pendulum_multiple_controller_options.py
+++ examples/demo_simple_pendulum_multiple_controller_options.py
@@ -11,7 +11,7 @@ from pyro.dynamic  import pendulum
 from pyro.control  import linear
 from pyro.control  import nonlinear
 from pyro.planning import plan
-from pyro.planning import randomtree
+from pyro.analysis import simulation
 ###############################################################################
 
 sys  = pendulum.SinglePendulum()
@@ -21,21 +21,41 @@ sys  = pendulum.SinglePendulum()
 # Planning
 
 traj   = plan.load_trajectory('pendulum_rrt.npy')
-q_goal = np.array([3.14])
+q_goal = np.array([-3.14])
 
 ###############################################################################
 
+# P
+kp = 5
+kd = 0
+ki = 0
+p_ctl      = linear.JointPID(sys, kp , ki, kd)
+p_ctl.rbar = q_goal
 
+# PD
+kp = 5
+kd = 2
+ki = 0
+pd_ctl      = linear.JointPID(sys, kp , ki, kd)
+pd_ctl.rbar = q_goal
 
+# PID
+kp = 5
+kd = 2
+ki = 1
+pid_ctl      = linear.JointPID(sys, kp , ki, kd)
+pid_ctl.rbar = q_goal
 
 # Computed Torque
 ctc_ctl      = nonlinear.ComputedTorqueController( sys )
 ctc_ctl.rbar = q_goal
+ctc_ctl.w0   = 2.0
+ctc_ctl.zeta = 0.8 
 
 # Sliding Mode 
 sld_ctl      = nonlinear.SlidingModeController( sys )
-sld_ctl.lam  = 5
-sld_ctl.gain = 2
+sld_ctl.lam  = 1
+sld_ctl.gain = 5
 sld_ctl.rbar = q_goal
 
 # OpenLoop with traj
@@ -44,25 +64,27 @@ traj_ctl = plan.OpenLoopController( traj )
 # Computed Torque with traj
 traj_ctc_ctl      = nonlinear.ComputedTorqueController( sys , traj )
 traj_ctc_ctl.rbar = q_goal
+traj_ctc_ctl.w0   = 2.0
+traj_ctc_ctl.zeta = 0.8 
 
 # Sliding Mode with traj
 traj_sld_ctl      = nonlinear.SlidingModeController( sys , traj )
-traj_sld_ctl.lam  = 5
-traj_sld_ctl.gain = 2
+traj_sld_ctl.lam  = 1
+traj_sld_ctl.gain = 5
 traj_sld_ctl.rbar = q_goal
 
 ###############################################################################
 
 # Controller selection
 
-ctl = 
-ctl = 
-ctl = 
-ctl = ctc_ctl
-ctl = sld_ctl
-ctl = traj_ctl
-ctl = traj_ctc_ctl 
-ctl = traj_sld_ctl
+ctl = p_ctl
+#ctl = pd_ctl
+#ctl = pid_ctl
+#ctl = ctc_ctl
+#ctl = sld_ctl
+#ctl = traj_ctl
+#ctl = traj_ctc_ctl 
+#ctl = traj_sld_ctl
 
 
 ###############################################################################
@@ -71,6 +93,10 @@ ctl = traj_sld_ctl
 cl_sys = ctl + sys
 
 # Simultation
-x_start  = np.array([0.1,0])
-cl_sys.plot_trajectory(x_start, 5, 10001, 'euler')
+q0 = 0
+tf = 10
+cl_sys.sim = simulation.CLosedLoopSimulation( cl_sys , tf , tf * 1000 + 1 , 'euler' )
+cl_sys.sim.x0 = np.array([q0,0])
+cl_sys.sim.compute()
+cl_sys.sim.plot('xu')
 cl_sys.animate_simulation()
--- examples/simple_pendulum_with_pid.py
+++ examples/simple_pendulum_with_pid.py
@@ -7,8 +7,9 @@ Created on Fri Nov 16 12:05:08 2018
 ###############################################################################
 import numpy as np
 ###############################################################################
-from pyro.dynamic import pendulum
-from pyro.control import linear
+from pyro.dynamic  import pendulum
+from pyro.control  import linear
+from pyro.analysis import simulation
 ###############################################################################
 
 sys  = pendulum.SinglePendulum()
@@ -28,6 +29,8 @@ cl_sys = ctl + sys
 
 # Simultation
 x_start  = np.array([0,0])
-cl_sys.plot_phase_plane_trajectory( x_start , 10 )
+
+cl_sys.sim = simulation.CLosedLoopSimulation( cl_sys , 20 , 20001 , 'euler' )
+cl_sys.sim.compute()
 cl_sys.sim.plot('xu')
 cl_sys.animate_simulation()
