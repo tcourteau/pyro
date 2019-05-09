05c82e70e203a0df84a20048a47021d09a61f601
Alexandre Girard <alx87grd@gmail.com>
2019-05-09 16:20:36 -0400

    kinematic controller working

--- control/robotcontrollers.py
+++ control/robotcontrollers.py
@@ -9,6 +9,7 @@ Created on Wed May  8 12:43:21 2019
 import numpy as np
 ###############################################################################
 from pyro.control import controller
+from pyro.dynamic import manipulator
 ###############################################################################
 
 
@@ -20,8 +21,7 @@ class RobotController( controller.StaticController ) :
     """     """
     ############################
     def __init__(self, dof = 1):
-        
-        self.dof = dof
+        """ """
 
         # Dimensions
         self.k = dof 
@@ -33,12 +33,15 @@ class RobotController( controller.StaticController ) :
         # Label
         self.name = 'Robot Controller'
         
+        # DoF
+        self.dof = dof
+        
     #############################
     def x2q( self, x ):
         """ from state vector (x) to angle and speeds (q,dq) """
         
-        q  = x[ 0        : self.dof ]
-        dq = x[ self.dof : self.n   ]
+        q  = x[ 0        :     self.dof   ]
+        dq = x[ self.dof : 2 * self.dof   ]
         
         return [ q , dq ]
         
@@ -66,7 +69,7 @@ class JointPID( RobotController ) :
     def __init__(self, dof = 1, kp = 1, ki = 0, kd = 0):
         """ """
         
-        RobotController.__init__( dof )
+        RobotController.__init__( self , dof )
         
         # Label
         self.name = 'Joint PID Controller'
@@ -147,7 +150,7 @@ class EndEffectorPID( RobotController ) :
         self.J       = manipulator.J
         self.e       = manipulator.e # nb of effector dof
         
-        RobotController.__init__( manipulator.dof )
+        RobotController.__init__( self , manipulator.dof )
         
         # Label
         self.name = 'End-Effector PID Controller'
@@ -207,3 +210,142 @@ class EndEffectorPID( RobotController ) :
         self.e_int =  ie
         
         return u
+    
+    
+###############################################################################
+# Kinematic Controller
+###############################################################################
+        
+class EndEffectorKinematicController( RobotController ) :
+    """ 
+    Kinematic effector coordinates controller using the Jacobian of the system
+    ---------------------------------------
+    r  : reference signal vector  e   x 1
+    y  : sensor signal vector     dof x 1
+    u  : control inputs vector    dof x 1
+    t  : time                     1   x 1
+    ---------------------------------------
+    u = c( y , r , t ) = J(q)^T *  [ (r - r_robot(q)) * k ]
+
+    """
+    
+    ############################
+    def __init__(self, manipulator, k = 0):
+        """ """
+        
+        # Using from model
+        self.fwd_kin = manipulator.forward_kinematic_effector
+        self.J       = manipulator.J
+        self.e       = manipulator.e # nb of effector dof
+        
+        # Dimensions
+        self.dof = manipulator.dof
+        self.k   = self.dof 
+        self.m   = self.dof
+        self.p   = self.dof
+        
+        controller.StaticController.__init__(self, self.k, self.m, self.p)
+
+        # Label
+        self.name = 'End Effector Kinematic Controller'
+        
+        # Gains
+        self.gains = np.ones( self.dof  ) * k
+        
+    
+    #############################
+    def c( self , y , r , t = 0 ):
+        """ 
+        Feedback static computation u = c(y,r,t)
+        
+        INPUTS
+        y  : sensor signal vector     p x 1
+        r  : reference signal vector  k x 1
+        t  : time                     1 x 1
+        
+        OUPUTS
+        u  : control inputs vector    m x 1
+        
+        """
+        
+        #u = np.zeros(self.m) 
+        
+        # Feedback from sensors
+        q = y
+        
+        # Jacobian computation
+        J = self.J( q )
+        
+        # Ref
+        r_desired   = r
+        r_actual    = self.fwd_kin( q )
+        
+        # Error
+        e  = r_desired - r_actual
+        
+        # Effector space PID
+        dr_r = e * self.gains
+        
+        # From effector force to joint torques
+        if self.dof == self.e:
+            dq_r = np.dot( np.linalg.inv( J ) , dr_r )
+        else:
+            #TODO
+            pass
+        
+        return dq_r
+    
+    
+    
+'''
+#################################################################
+##################          Main                         ########
+#################################################################
+'''
+
+
+if __name__ == "__main__":     
+    """ MAIN TEST """
+    
+   
+    robot       = manipulator.TwoLinkManipulator()
+    
+    kp = 10
+    kd = 0
+    
+    joint_pid         = JointPID(2, kp , 0 , kd)
+    joint_pid.rbar    = np.array([0,0])
+    
+    kp = 200
+    kd = 0
+    
+    effector_pid      = EndEffectorPID( robot , kp , 0 , kd)
+    effector_pid.rbar = np.array([0.5,0.5])
+
+    #closed_loop_sys = joint_pid + robot
+    closed_loop_sys = effector_pid + robot
+    
+    # Simulation
+    x0              = np.array([2,2,0,0])
+    
+    closed_loop_sys.compute_trajectory( x0 )
+    closed_loop_sys.animate_simulation()
+    closed_loop_sys.sim.plot('xu')
+    
+    ##############################################################
+    
+    kin_robot = manipulator.SpeedControlledManipulator( robot )
+    
+    k = 1
+    kin_ctl = EndEffectorKinematicController( kin_robot , k )
+    
+    kin_ctl.rbar = np.array([0.5,0.5])
+    
+    kin_closed_loop = kin_ctl + kin_robot
+    
+    x0              = np.array([2,2])
+    
+    kin_closed_loop.compute_trajectory( x0 )
+    kin_closed_loop.animate_simulation()
+    kin_closed_loop.sim.plot('xu')
+    
